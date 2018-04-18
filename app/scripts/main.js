1c33a451fd2a0a6d309a94fdf4904d2f57ff08a2
Sychenko Dmytro <dmitrosichenko@ukr.net>
2018-04-18 11:40:15 +0300

    push repo

---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  function addZero(str, max) {
+    str = str.toString();
+    return str.length < max ? addZero("0" + str, max) : str;
+  };
+
+  function setTimer(startingDate, selector) {
+    let countDown = setInterval(function() {
+      let now = new Date().getTime(),
+        difference = startingDate - now,
+        days = addZero(Math.floor(difference / (1000 * 60 * 60 * 24)), 2),
+        hours = addZero(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), 2),
+        minutes = addZero(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)), 2),
+        seconds = addZero(Math.floor((difference % (1000 * 60)) / 1000), 2);
+      $(selector).find('.days').text(days);
+      $(selector).find('.hours').text(hours);
+      $(selector).find('.minutes').text(minutes);
+      $(selector).find('.seconds').text(seconds);
+      if (difference < 0) {
+        clearInterval(countDown);
+        $(selector).find('.timer').remove();
+      }
+    }, 1000);
+  };
+  setTimer(1546812001000, '#timer0');
+  setTimer(1524236400000, '#timer1');
+  setTimer(1523869200000, '#timer2');
+  setTimer(1546812001000, '#timer3');
+
+  // slider setting
+  $('.review-slider').slick({
+  	autoplay: true,
+  	slidesToShow: 2,
+  	rows: 0,
+  	nextArrow: '<button type="button" class="slick-next icon-arrow"></button>',
+  	prevArrow: '<button type="button" class="slick-prev icon-arrow"></button>',
+  });
+  $('.last-news-slider').slick({
+  	autoplay: true,
+  	slidesToShow: 3,
+  	rows: 0,
+  	nextArrow: '<button type="button" class="slick-next icon-arrow"></button>',
+  	prevArrow: '<button type="button" class="slick-prev icon-arrow"></button>',
+  })
+})
